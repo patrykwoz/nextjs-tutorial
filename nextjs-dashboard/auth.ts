37c12848316213f52e43817bfe171f7ad2cf6130
root <root@PATRYK-NFHTHTE0>
2024-07-22 15:28:29 -0400

    Finish tutorial

---
+++ nextjs-dashboard/auth.ts
@@ -0,0 +1,46 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+
+import postgres from 'postgres';
+const sql = postgres('postgresql://postgres:postgres@localhost:5432/nextjs-dashboard');
+
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+        const user = await sql`SELECT * FROM users WHERE email=${email}`;
+        console.log('user log', user[0])
+        return user[0];
+    } catch (error) {
+        console.error('Failed to fetch user:', error);
+        throw new Error('Failed to fetch user.');
+    }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials);
+
+                if (parsedCredentials.success) {
+                    const { email, password } = parsedCredentials.data;
+                    const user = await getUser(email);
+                    if (!user) return null;
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if (passwordsMatch) return user;
+                }
+
+                console.log('Invalid credentials');
+                return null;
+            },
+        }),
+    ],
+});
